afe0c48d469e821720f589b698a9495210c2592f
adams-creatives <owolabioshoala@gmail.com>
2024-06-01 05:38:51 +0100

    Build React app and update server to serve static files

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 const port = process.env.PORT || 5000;
 
 const cors = require("cors");
@@ -9,10 +10,13 @@ const pool = require("./db");
 app.use(cors());
 app.use(express.json()); //req.body
 
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, "../client/build")));
+
 //ROUTES//
-app.get("/", (req, res) => {
-  res.send("Welcome to the Todo App API");
-});
+// app.get("/", (req, res) => {
+//   res.send("Welcome to the Todo App API");
+// });
 
 
 //create a todo
@@ -87,6 +91,12 @@ app.delete("/todos/:id", async (req, res) => {
     console.log(err.message);
   }
 });
+
+// All other GET requests not handled before will return the React app
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build", "index.html"));
+});
+
 
 app.listen(port, () => {
   console.log(`server has started on port ${port}`);
